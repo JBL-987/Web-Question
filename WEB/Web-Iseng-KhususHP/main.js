86bc8626bf3e7c77194f1abcf5f594e2b7ab631c
Loi <129076315+JBL-987@users.noreply.github.com>
2023-09-15 23:50:08 +0700

    Add files via upload

---
+++ WEB/Web-Iseng-KhususHP/main.js
@@ -0,0 +1,11 @@
+while (true) {
+  let ya = confirm("DO YOU WANT TO OPEN THIS WEB?");
+
+  if (ya) {
+    alert("THANK YOU");
+    break;
+  } else {
+    window.open("https://www.youtube.com/watch?v=Utxb-kzvFZU&pp=ygUHaGFoIGdheQ%3D%3D");
+    break;
+  }
+}
---
+++ WEB/Web-Iseng/main.js
@@ -0,0 +1,15 @@
+function opensametab() {
+  window.location.href = "https://www.youtube.com/watch?v=Utxb-kzvFZU&pp=ygUHaGFoIGdheQ%3D%3D";
+}
+
+while (true) {
+  let ya = confirm("DO YOU WANT TO OPEN THIS WEB?");
+
+  if (ya) {
+    break;
+  } else {
+    opensametab();
+    break;
+  }
+}
+ alert("THANK YOU");
